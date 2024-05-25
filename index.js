d2c463031d91af21058b61fafd0216cf99976a6f
lucas dionisio <144950155+lucasdionisio1@users.noreply.github.com>
2024-05-24 23:41:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.forms['mf'].elements['result'].value = '';
+}
+
+function appendToDisplay(value) {
+    document.forms['mf'].elements['result'].value += value;
+}
+
+function calculateResult() {
+    try {
+        const expression = document.forms['mf'].elements['result'].value.replace(/x/g, '*');
+        document.forms['mf'].elements['result'].value = eval(expression);
+    } catch (e) {
+        document.forms['mf'].elements['result'].value = 'Erro';
+    }
+}
